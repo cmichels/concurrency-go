8793132c5d27f578256ddf5cdbe76440d9608899
Chris Michels <chris.j.michels@gmail.com>
2024-10-08 06:40:25 -0400

    initial producer setup

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -1,25 +1,53 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 const (
 	numberOfPizzas = 10
 )
 
-
 var pizzasMade, pizzasFailed, total int
 
-
 type Producer struct {
-  data chan PizzaOrder
-  quit chan chan error
+	data chan PizzaOrder
+	quit chan chan error
 }
 
-
 type PizzaOrder struct {
-  orderNumber int
-  message string
-  success bool
+	orderNumber int
+	message     string
+	success     bool
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+
+	return <-ch
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	for {
+     
+	}
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Cyan("the pizzeria is open for business")
+	color.Cyan("---------------------------------")
+
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	go pizzeria(pizzaJob)
 
 }
