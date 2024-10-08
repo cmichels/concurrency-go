d19d597825acb0ae5aff792d284e0b1d68c728bf
Chris Michels <chris.j.michels@gmail.com>
2024-10-08 05:53:47 -0400

    fix race condition

--- mutex-example/main.go
+++ mutex-example/main.go
@@ -5,25 +5,28 @@ import (
 	"sync"
 )
 
-
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string)  {
-  defer wg.Done()
-  msg = s
+func updateMessage(s string, m *sync.Mutex) {
+	defer wg.Done()
+  m.Lock() 
+	msg = s
+  m.Unlock() 
 }
 func main() {
-  
 
-  msg = "hello, worlds"
 
-  wg.Add(2)
-  go updateMessage("hello one")
-  go updateMessage("hello two")
 
-  wg.Wait()
+	msg = "hello, worlds"
+  var mutex sync.Mutex
 
-  fmt.Println("msg:", msg)
-  
+	wg.Add(2)
+	go updateMessage("hello one", &mutex)
+	go updateMessage("hello two", &mutex)
+
+	wg.Wait()
+
+	fmt.Println("msg:", msg)
+
 }
