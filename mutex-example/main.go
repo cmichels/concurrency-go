12366a3d0e1f71198e8a627026f41076b94eb708
Chris Michels <chris.j.michels@gmail.com>
2024-10-08 05:59:17 -0400

    add failing test

--- mutex-example/main.go
+++ mutex-example/main.go
@@ -8,25 +8,47 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string, m *sync.Mutex) {
+func updateMessage(s string) {
 	defer wg.Done()
-  m.Lock() 
 	msg = s
-  m.Unlock() 
 }
 func main() {
 
 
 
 	msg = "hello, worlds"
-  var mutex sync.Mutex
 
 	wg.Add(2)
-	go updateMessage("hello one", &mutex)
-	go updateMessage("hello two", &mutex)
+	go updateMessage("hello one")
+	go updateMessage("hello two")
 
 	wg.Wait()
 
 	fmt.Println("msg:", msg)
 
 }
+
+
+
+// func updateMessage(s string, m *sync.Mutex) {
+// 	defer wg.Done()
+//   m.Lock() 
+// 	msg = s
+//   m.Unlock() 
+// }
+// func main() {
+//
+//
+//
+// 	msg = "hello, worlds"
+//   var mutex sync.Mutex
+//
+// 	wg.Add(2)
+// 	go updateMessage("hello one", &mutex)
+// 	go updateMessage("hello two", &mutex)
+//
+// 	wg.Wait()
+//
+// 	fmt.Println("msg:", msg)
+//
+// }
