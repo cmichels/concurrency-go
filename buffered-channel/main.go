8249d17fcd3d87d14a95e027661fbff6c05d241b
Chris Michels <chris.j.michels@gmail.com>
2024-10-09 09:15:40 -0400

    impl buffered chan

--- buffered-channel/main.go
+++ buffered-channel/main.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
+func listenToChan(ch chan int) {
+	for {
+		i := <-ch
+
+		fmt.Println("recieved ", i, " from channel")
+		time.Sleep(1 * time.Second)
+	}
+}
 func main() {
-  
+
+	ch := make(chan int, 10)
+
+	go listenToChan(ch)
+
+	for i := 0; i < 100; i++ {
+		fmt.Println("sedning i ", i, " to chan")
+		ch <- i
+		fmt.Println("sent ", i, " to chan")
+
+	}
+
+	fmt.Println("done")
+	close(ch)
+
 }
