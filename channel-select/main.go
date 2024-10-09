599169f53e2104d038ece330c717091964c4bec0
Chris Michels <chris.j.michels@gmail.com>
2024-10-09 08:46:07 -0400

    select cases are random when equal

--- channel-select/main.go
+++ channel-select/main.go
@@ -1,7 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
+func serverOne(ch chan string) {
+
+	for {
+		time.Sleep(6 * time.Second)
+		ch <- "this is from sever one"
+	}
+}
+
+func serverTwo(ch chan string) {
+
+	for {
+		time.Sleep(3 * time.Second)
+		ch <- "this is from sever two"
+	}
+}
 
 func main() {
-  
+
+	fmt.Println("select with channels")
+	fmt.Println("--------------------")
+
+	chOne := make(chan string)
+	chTwo := make(chan string)
+
+	go serverOne(chOne)
+	go serverTwo(chTwo)
+
+	for {
+		select {
+		case s1 := <-chOne:
+			fmt.Println("case 1:", s1)
+		case s2 := <-chOne:
+			fmt.Println("case 2:", s2)
+		case s3 := <-chTwo:
+			fmt.Println("case 3: ", s3)
+		case s4 := <-chTwo:
+			fmt.Println("case 4: ", s4)
+		default:
+			// used to avoid deadlock
+      fmt.Println("nothing listening")
+		}
+	}
+
 }
