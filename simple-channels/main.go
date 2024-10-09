d27cd62484d26441141150cbdb8e3caad0c5d47a
Chris Michels <chris.j.michels@gmail.com>
2024-10-09 08:05:01 -0400

    run channel ping pong

--- simple-channels/main.go
+++ simple-channels/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s := <-ping
+		pong <- fmt.Sprintf("%s", strings.ToUpper(s))
+	}
+}
 func main() {
-  
+
+	ping := make(chan string)
+	pong := make(chan string)
+	go shout(ping, pong)
+
+	fmt.Println("enter ping")
+	for {
+		fmt.Print("-> ")
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if userInput == strings.ToLower("q") {
+			break
+		}
+
+		ping <- userInput
+
+		response := <-pong
+		fmt.Println("response: ", response)
+	}
+
+	close(ping)
+	close(pong)
+
 }
