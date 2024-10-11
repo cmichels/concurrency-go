74354456c52143648e4bfc2143aaae339f3cca17
Chris Michels <chris.j.michels@gmail.com>
2024-10-11 05:39:55 -0400

    setup db init

--- sub-service/cmd/web/main.go
+++ sub-service/cmd/web/main.go
@@ -1,25 +1,90 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const (
 	webPort = "80"
 )
 
-// main function 
+// main function
 func main() {
-  
 
-  // connect to database
+	// connect to database
+	db := initDB()
 
-  // create sessions
+	db.Ping()
 
-  // create channels
+	// create sessions
 
-  // create waitgroup
+	// create channels
 
-  // setup app config
+	// create waitgroup
 
-  // set mail
+	// setup app config
 
-  // listen for web
+	// set mail
 
+	// listen for web
+
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("cannot connect to db")
+	}
+
+	return conn
+
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+
+		if err != nil {
+			log.Println("postgres not yet ready")
+		} else {
+			log.Println("connected to db")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("waiting for 1 second")
+		time.Sleep(1 * time.Second)
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
