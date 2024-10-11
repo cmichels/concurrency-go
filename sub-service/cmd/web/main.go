5335372ead8129da24dc53e8774c7af966124f67
Chris Michels <chris.j.michels@gmail.com>
2024-10-11 05:23:33 -0400

    add dependencies

--- sub-service/cmd/web/main.go
+++ sub-service/cmd/web/main.go
@@ -1,5 +1,25 @@
 package main
 
+const (
+	webPort = "80"
+)
+
+// main function 
 func main() {
   
+
+  // connect to database
+
+  // create sessions
+
+  // create channels
+
+  // create waitgroup
+
+  // setup app config
+
+  // set mail
+
+  // listen for web
+
 }
